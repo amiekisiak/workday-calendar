de9c660eda4694c584b5ed306212e1476f571ec1
amiekisiak <117371691+amiekisiak@users.noreply.github.com>
2023-01-23 23:37:33 +0000

    updated Readme file

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-# Third-Party APIs: Work Day Scheduler
+# workday-planner
 
-## Your Task
+
+## Description
 
 Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
 You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
 
-## User Story
+## Installation
 
 ```md
 AS AN employee with a busy schedule
@@ -14,7 +15,7 @@ I WANT to add important events to a daily planner
 SO THAT I can manage my time effectively
 ```
  
-## Acceptance Criteria
+## Usage
 
 The app should:
 
@@ -35,53 +36,10 @@ The following animation demonstrates the application functionality:
 ![A user clicks on slots on the color-coded calendar and edits the events.](./images/05-third-party-apis-homework-demo.gif)
 
 
-## Grading Requirements
+## Screenshot
 
 This homework is graded based on the following criteria: 
 
-### Technical Acceptance Criteria: 40%
+## License
 
-* Satisfies all of the above acceptance criteria plus the following:
-
-  * Uses a date utility library to work with date and time
-
-### Deployment: 32%
-
-* Application deployed at live URL
-
-* Application loads with no errors
-
-* Application GitHub URL submitted
-
-* GitHub repo contains application code
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate
-
-* Application user interface style is clean and polished
-
-* Application resembles the mock-up functionality provided in the homework instructions
-
-### Repository Quality: 13%
-
-* Repository has a unique name
-
-* Repository follows best practices for file structure and naming conventions
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages
-
-* Repository contains quality README file with description, screenshot, and link to deployed application
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application
-
-* The URL of the GitHub repository, with a unique name and a README describing the project
-
----
-© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+Licensed under the MIT license.
--- starter/script.js
+++ starter/script.js
@@ -6,9 +6,13 @@ const timeBlocks = document.querySelectorAll('.time-block-container');
 // Save task to local storage when save button is clicked
 timeBlocks.forEach(block => {
     block.querySelector('.time-block-saveBtn').addEventListener('click', e => {
+        // Get the text value of the task
         const textValue = e.target.parentNode.querySelector('.time-block-description').value;
+        // Get the hour value of the block
         const hour = block.querySelector(".hour span").textContent;
+        // Format the hour value to HH format
         let hourValue = moment(hour, "h:mm A").format("HH");
+        // Save the text value and hour value to local storage
         localStorage.setItem(hourValue, textValue);
     });
 });
@@ -16,15 +20,18 @@ timeBlocks.forEach(block => {
 
 // Load tasks from local storage and update color of blocks based on time
 
-
 function updateTimeBlocks() {
     now = moment();
     currentDay.innerHTML = now.format("dddd, MMMM Do YYYY, h:mm:ss a");
     timeBlocks.forEach(block => {
+        // Get the hour value of the block
         const hour = block.querySelector(".hour span").textContent;
+        // Format the hour value to HH format
         let hourValue = moment(hour, "h:mm A").format("HH");
         let hourMoment = moment(hourValue, "HH");
+        // Remove all past, present and future classes
         block.classList.remove("past", "present", "future");
+        // Add class based on current time
         if (hourMoment.isBefore(now, 'hour')) {
             block.classList.add("past");
         } else if (hourMoment.isSame(now, 'hour')) {
@@ -35,22 +42,29 @@ function updateTimeBlocks() {
     });
 }
 
+// Load tasks from local storage and update color of blocks when the page loads
 
 window.onload = function () {
     timeBlocks.forEach(block => {
+        // Get the hour value of the block
         const hour = block.querySelector(".hour span").textContent;
+        // Format the hour value to HH format
         let hourValue = moment(hour, "h:mm A").format("HH");
+        // Load the text value from local storage and set it to the text area
         block.querySelector(".time-block-description").value = localStorage.getItem(hourValue);
     });
     updateTimeBlocks();
 }
 
 // Add a clear button
-    
-$("#clearBlocksBtn").click(function() {
-    console.log($("textArea"))
+
+$("#clearBlocksBtn").click(function (event) {
+    event.preventDefault;
+    // Clear the text area and local storage
     $("textArea").val("");
     localStorage.clear();
+
+
 });
-   
+
 setInterval(updateTimeBlocks, 60000);
